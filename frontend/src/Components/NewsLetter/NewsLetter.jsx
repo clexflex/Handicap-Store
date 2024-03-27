e4207038a446e452fbec229f96376ce1f4d48eae
yashraj <yashrajjghosalkar@gmail.com>
2024-03-27 16:19:32 +0530

    newsLetter,new Collection implemented

--- frontend/src/Components/NewsLetter/NewsLetter.jsx
+++ frontend/src/Components/NewsLetter/NewsLetter.jsx
@@ -3,7 +3,12 @@ import './NewsLetter.css'
 const NewsLetter = () => {
   return (
     <div className='newsletter'>
-        
+        <h1>Get Exclusive Offers on You Email</h1>
+        <p>Suscribe to our news letter and stay updated</p>
+        <div>
+            <input type="email" placeholder='Your email id' />
+            <button>Subscribe</button>
+        </div>
     </div>
   )
 }
