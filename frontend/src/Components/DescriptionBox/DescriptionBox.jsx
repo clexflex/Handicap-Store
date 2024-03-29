5aee601411d5fee5cfbc26428b4b1dd333ec9b84
yashraj <yashrajjghosalkar@gmail.com>
2024-03-29 23:35:17 +0530

    descriptionbox,relatedproduct created

---
+++ frontend/src/Components/DescriptionBox/DescriptionBox.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import './DescriptionBox.css'
+
+const DescriptionBox = () => {
+  return (
+    <div className='descriptionbox'>
+        <div className="descriptionbox-navigator">
+            <div className="descriptionbox-nav-box">Description</div>
+            <div className="descriptionbox-nav-box fade">Reviews (122)</div>
+        </div>
+        <div className="descriptionbox-description">
+            <p> 
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aliquid repellendus dolorem non cupiditate eum a sed asperiores nihil tempora iusto debitis, delectus illo similiqueit. Aliquid repellendus dolorem non cupiditate eum a sed asperiores nihil tempora iusto debitis, delectus illo similiqueit. Aliquid repellendus dolorem non cupiditate eum a sed asperiores nihil tempora iusto debitis, delectus illo similique, perferendis libero facilis, harum ipsa odio!
+            
+            </p>
+            <p> 
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi, sed veniam. Facere suscipit dolores quidem officia aliquam vitae, incidunt neque saepe dolorum nesciunt impedit deserunt, nemo blanditiis eveniet fuga maxime.
+            </p>
+        </div>
+    </div>
+  )
+}
+
+export default DescriptionBox
