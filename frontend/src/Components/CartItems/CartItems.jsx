8e31a72c99bde415562d6b5a7504574a7c17dfcb
yashraj <yashrajjghosalkar@gmail.com>
2024-03-31 15:42:31 +0530

    Complete Responsive Changes

--- frontend/src/Components/CartItems/CartItems.jsx
+++ frontend/src/Components/CartItems/CartItems.jsx
@@ -7,7 +7,7 @@ const CartItems = () => {
     const {getTotalCartAmount,all_product,cartItems,removeFromCart} = useContext(ShopContext);
   return (
     <div className='cartitems'>
-        <div className="cartitem-format-main">
+        <div className="cartitems-format-main">
             <p>Products</p>
             <p>Title</p>
             <p>Price</p>
@@ -19,12 +19,12 @@ const CartItems = () => {
        {all_product.map((e)=>{
         if(cartItems[e.id]>0){
             return  <div>
-                        <div className="cartitems-format cartitem-format-main">
+                        <div className="cartitems-format cartitems-format-main">
                             <img src={e.image} alt="" className='carticon-product-icon' />
                             <p>{e.name}</p>
                             <p>₹{e.new_price}</p>
                             <button className='cartitems-quantity'>{cartItems[e.id]}</button>
-                            <p>{e.new_price*cartItems[e.id]}</p>
+                            <p>₹{e.new_price*cartItems[e.id]}</p>
                             <img className='cartitems-remove-icon' src={remove_icon} onClick={()=>{removeFromCart(e.id)}} alt="" />
                         </div>
                         <hr />
