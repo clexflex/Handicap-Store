3fb19178012596dc1137814d4403a1cc330df617
yashraj <yashrajjghosalkar@gmail.com>
2024-04-18 15:30:31 +0530

    new update

--- admin/src/Components/Navbar/Navbar.jsx
+++ admin/src/Components/Navbar/Navbar.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import './Navbar.css'
+import navlogo from '../../assets/nav-logo.svg'
+import navProfile from '../../assets/nav-profile.svg'
 const Navbar = () => {
   return (
     <div className='navbar'>
-      Navbar
+       <img src={navlogo} alt="" className='nav-logo'/>
+        <img src={navProfile} alt="" className='nav-profile'/>
     </div>
   )
 }
