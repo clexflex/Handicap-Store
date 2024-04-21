d8ef90e886fe39747e6c84519fa9d9e61762a8fd
yashraj <yashrajjghosalkar@gmail.com>
2024-04-22 01:06:59 +0530

    admin navbar,sidebar and add product implemented

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
-import Navbar from './Components/Navbar/Navbar';
+import Navbar from './Components/navbar/Navbar';
+import Admin from './Pages/Admin/Admin';
 
 const App = () => {
   return (
     <div>
       <Navbar />
+      <Admin />
     </div>
   )
 }
